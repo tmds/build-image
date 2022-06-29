49869ea4ee80dca9c38439716fa3f9d4b035c587
Tom Deseyn <tom.deseyn@gmail.com>
2022-06-29 14:52:59 +0200

    Use global.json file if present to determine SDK version.

---
+++ src/build-image/GlobalJsonReader.cs
@@ -0,0 +1,56 @@
+using System.Text.Json;
+
+class GlobalJson
+{
+    public Version? SdkVersion { get; set; }
+}
+
+class GlobalJsonReader
+{
+    public static GlobalJson? ReadGlobalJson(string path)
+    {
+        path = Path.GetFullPath(path);
+        string root = Path.GetPathRoot(path)!;
+
+        do
+        {
+            string globalJsonFileName = Path.Combine(path, "global.json");
+            System.Console.WriteLine($"Checking {globalJsonFileName}");
+            if (File.Exists(globalJsonFileName))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                GlobalJsonContent? content;
+                try
+                {
+                    content = JsonSerializer.Deserialize<GlobalJsonContent>(File.ReadAllText(globalJsonFileName), options);
+                }
+                catch
+                {
+                    return null;
+                }
+                GlobalJson globalJson = new();
+                if (content?.Sdk?.Version is string v && Version.TryParse(v, out Version? result))
+                {
+                    globalJson.SdkVersion = result;
+                }
+                return globalJson;
+            }
+            path = Path.GetDirectoryName(path)!;
+        } while (root != path);
+
+        return null;
+    }
+
+    record GlobalJsonSdk
+    {
+        public string? Version { get; set; }
+    }
+
+    record GlobalJsonContent
+    {
+        public GlobalJsonSdk? Sdk { get; set; }
+    }
+}
