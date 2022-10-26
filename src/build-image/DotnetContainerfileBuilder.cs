0d89f8c61609cb4ee62f275d1bd43e8ace1f9776
Tom Deseyn <tom.deseyn@gmail.com>
2022-10-26 16:11:33 +0200

    Fix NuGet package caching. (#30)

--- src/build-image/DotnetContainerfileBuilder.cs
+++ src/build-image/DotnetContainerfileBuilder.cs
@@ -17,8 +17,10 @@ class DotnetContainerfileBuilder
     {
         const int ContainerUid = 1001;
         const int ContainerGid = 0;
+        const string BuildHomeDir = "/home/build";
         const string HomeDir = "/home/app";
         const string AppDir = "/app";
+        const string TargetRoot = "/rootfs";
 
         string fromImage = options.RuntimeImage ?? throw new ArgumentNullException(nameof(options.RuntimeImage));
         string buildImage = options.SdkImage ?? throw new ArgumentNullException(nameof(options.SdkImage));
@@ -29,26 +31,31 @@ class DotnetContainerfileBuilder
         sb.AppendLine($"ARG UID={ContainerUid}");
         sb.AppendLine($"ARG GID={ContainerGid}");
         sb.AppendLine($"");
+
         sb.AppendLine($"# Publish application");
         sb.AppendLine($"FROM {buildImage} AS build-env");
         sb.AppendLine($"ARG UID");
         sb.AppendLine($"ARG GID");
         sb.AppendLine("USER 0");
+
         // Ensure uid and gid are known by the target image.
         sb.AppendLine($"COPY --from={fromImage} /etc/passwd /etc/group /scratch/etc");
-        sb.AppendLine($"RUN grep \":$GID:\" /scratch/etc/group || echo \"app:x:$GID:\" >>/scratch/etc/group && mkdir -p /rootfs/etc && cp /scratch/etc/group /rootfs/etc");
-        sb.AppendLine($"RUN grep \":x:$UID:\" /scratch/etc/passwd || echo \"app:x:$UID:$GID::{HomeDir}:/usr/sbin/nologin\" >>/scratch/etc/passwd && mkdir -p /rootfs/etc && cp /scratch/etc/passwd /rootfs/etc");
+        sb.AppendLine($"RUN grep \":$GID:\" /scratch/etc/group || echo \"app:x:$GID:\" >>/scratch/etc/group && mkdir -p {TargetRoot}/etc && cp /scratch/etc/group {TargetRoot}/etc && \\");
+        sb.AppendLine($"    grep \":x:$UID:\" /scratch/etc/passwd || echo \"app:x:$UID:$GID::{HomeDir}:/usr/sbin/nologin\" >>/scratch/etc/passwd && mkdir -p {TargetRoot}/etc && cp /scratch/etc/passwd {TargetRoot}/etc");
         // Create a home directory.
-        sb.AppendLine($"RUN mkdir -p /rootfs{HomeDir}");
+        sb.AppendLine($"RUN mkdir -p {TargetRoot}{HomeDir}");
+
         // Publish the application
+        sb.AppendLine($"ENV HOME={BuildHomeDir}");
         sb.AppendLine($"WORKDIR /src");
         sb.AppendLine($"COPY . ./");
         string relabel = options.SupportsCacheMountSELinuxRelabling ? ",Z" : "";
-        string cacheMount = options.SupportsCacheMount ? $"--mount=type=cache,id=nuget,target=${{HOME}}/.nuget/packages{relabel} " : "";
+        string cacheMount = options.SupportsCacheMount ? $"--mount=type=cache,id=nuget,target={BuildHomeDir}/.nuget/packages{relabel} " : "";
         sb.AppendLine($"RUN {cacheMount}dotnet restore {projectPath}");
-        sb.AppendLine($"RUN {cacheMount}dotnet publish --no-restore -c Release -o /rootfs/app {projectPath}");
+        sb.AppendLine($"RUN {cacheMount}dotnet publish --no-restore -c Release -o {TargetRoot}{AppDir} {projectPath}");
+ 
         // Ensure the application and home directory are owned by uid:gid.
-        sb.AppendLine($"RUN chgrp -R $GID /rootfs{AppDir} /rootfs{HomeDir} && chmod -R g=u /rootfs{AppDir} /rootfs{HomeDir} && chown -R $UID:$GID /rootfs{AppDir} /rootfs{HomeDir}");
+        sb.AppendLine($"RUN chgrp -R $GID {TargetRoot}{AppDir} {TargetRoot}{HomeDir} && chmod -R g=u {TargetRoot}{AppDir} {TargetRoot}{HomeDir} && chown -R $UID:$GID {TargetRoot}{AppDir} {TargetRoot}{HomeDir}");
         sb.AppendLine($"");
 
         sb.AppendLine($"# Build application image");
@@ -56,7 +63,7 @@ class DotnetContainerfileBuilder
         sb.AppendLine($"FROM {platformArch}{fromImage}");
         sb.AppendLine($"ARG UID");
         sb.AppendLine($"ARG GID");
-        sb.AppendLine($"COPY --from=build-env /rootfs /");
+        sb.AppendLine($"COPY --from=build-env {TargetRoot} /");
         sb.AppendLine($"USER $UID:$GID");
         sb.AppendLine("ENV ASPNETCORE_URLS=http://*:8080");
         sb.AppendLine($"ENV HOME={HomeDir}");
